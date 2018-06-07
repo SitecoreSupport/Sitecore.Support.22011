2c011206acc78e26e78b58a34e3088ed4baa6276
AntonKechashin <atk@sitecore.net>
2018-06-07 17:00:29 +0300

    Fix for 8.0 Update-7

---
+++ src/Sitecore.Support.22011/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.22011")]
+[assembly: AssemblyProduct("Sitecore.Support.22011")]
+[assembly: ComVisible(false)]
